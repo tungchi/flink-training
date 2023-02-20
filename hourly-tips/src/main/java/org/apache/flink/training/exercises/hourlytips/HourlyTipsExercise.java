9d554d275734b1ca4c3b568f747f5179eff00f70
yingkf <yingkf@runlion.com>
2023-02-20 13:33:03 +0800

    Add HourlyTipsExercise

--- hourly-tips/src/main/java/org/apache/flink/training/exercises/hourlytips/HourlyTipsExercise.java
+++ hourly-tips/src/main/java/org/apache/flink/training/exercises/hourlytips/HourlyTipsExercise.java
@@ -19,15 +19,20 @@
 package org.apache.flink.training.exercises.hourlytips;
 
 import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.training.exercises.common.datatypes.TaxiFare;
 import org.apache.flink.training.exercises.common.sources.TaxiFareGenerator;
-import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
+import org.apache.flink.util.Collector;
 
 /**
  * The Hourly Tips exercise from the Flink training.
@@ -73,21 +78,51 @@ public class HourlyTipsExercise {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // start the data generator
-        DataStream<TaxiFare> fares = env.addSource(source);
+        DataStream<TaxiFare> fares = env.addSource(source)
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<TaxiFare>forMonotonousTimestamps()
+                        .withTimestampAssigner((fare, t) -> fare.getEventTimeMillis()));
+        DataStream<Tuple3<Long, Long, Float>> sumDriverTips = fares.keyBy(fare -> fare.driverId)
+                .window(TumblingEventTimeWindows.of(Time.hours(1))).process(new TaxiRideMax());
 
         // replace this with your solution
-        if (true) {
-            throw new MissingSolutionException();
-        }
 
+        // if (true) {
+        // throw new MissingSolutionException();
+        // }
+        DataStream<Tuple3<Long, Long, Float>> hourlyMax =
+                sumDriverTips.windowAll(TumblingEventTimeWindows.of(Time.hours(1))).maxBy(2);
+        hourlyMax.addSink(sink);
         // the results should be sent to the sink that was passed in
         // (otherwise the tests won't work)
         // you can end the pipeline with something like this:
-
-        // DataStream<Tuple3<Long, Long, Float>> hourlyMax = ...
-        // hourlyMax.addSink(sink);
+        // DataStream<Tuple3<Long, Long, Float>> hourlyMax =
+        // fares.keyBy(taxifare -> taxifare.startTime)
+        // .flatMap(new RichFlatMapFunction<TaxiFare, Tuple3<Long, Long, Float>>() {
+        // @Override
+        // public void flatMap(TaxiFare value,
+        // Collector<Tuple3<Long, Long, Float>> out) throws Exception {
+        // out.collect(new Tuple3<>(value.getEventTimeMillis(), value.rideId,
+        // value.tip));
+        // }
+        // });
+        // hourlyMax.keyBy(value -> value.f0).addSink(sink);
 
         // execute the pipeline and return the result
         return env.execute("Hourly Tips");
     }
+
+    public static class TaxiRideMax
+            extends ProcessWindowFunction<TaxiFare, Tuple3<Long, Long, Float>, Long, TimeWindow> {
+
+        @Override
+        public void process(Long driverId, Context context, Iterable<TaxiFare> elements,
+                Collector<Tuple3<Long, Long, Float>> out) {
+            float sumTip = 0;
+            for (TaxiFare element : elements) {
+                sumTip = sumTip + element.tip;
+            }
+            out.collect(Tuple3.of(context.window().getEnd(), driverId, sumTip));
+        }
+    }
 }
+
